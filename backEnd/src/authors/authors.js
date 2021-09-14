705f572d9f7bae153d4db3fe7b01eea390045235
Istvan <steven.ford.j@gmail.com>
2021-09-14 16:48:43 +0300

    Working

--- backEnd/src/authors/authors.js
+++ backEnd/src/authors/authors.js
@@ -56,23 +56,26 @@ authorStrive.post("/", (req, res) => {
 authorStrive.put("/:postId", (req, res) => {
   const authors = JSON.parse(fs.readFileSync(authorJson));
   const index = authors.findIndex((author) => author._id === req.params.postId);
-  if (
-    index &&
-    req.body.name &&
-    req.body.surname &&
-    req.body.email &&
-    req.body.avatar &&
-    req.body.dateBirth
-  ) {
-    // SUCCESS POST
-    const updateAuthor = { ...authors[index], ...req.body };
-    authors[index] = updateAuthor;
-    //   save file
-    fs.writeFileSync(authorJson, JSON.stringify(authors));
-    //   response
-    res.status(201).send({ Yep: "its ok", body: updateAuthor });
+  if (index >= 0) {
+    if (
+      req.body.name &&
+      req.body.surname &&
+      req.body.email &&
+      req.body.avatar &&
+      req.body.dateBirth
+    ) {
+      // SUCCESS POST
+      const updateAuthor = { ...authors[index], ...req.body };
+      authors[index] = updateAuthor;
+      //   save file
+      fs.writeFileSync(authorJson, JSON.stringify(authors));
+      //   response
+      res.status(201).send({ Yep: "its ok", body: updateAuthor });
+    } else {
+      res.status(401).send({ Bad: "Request" });
+    }
   } else {
-    res.status(400).send({ NotToday: "Bad request" });
+    res.status(400).send({ Nope: "Bad request" });
   }
 });
 // delete
